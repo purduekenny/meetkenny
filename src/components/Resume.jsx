7b8ab182c4a78bcb6ea8406d0df800d992539ca1
Kenneth Wilson <kenny@meetkenny.com>
2021-04-17 00:02:40 -0400

    update title

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -18,7 +18,7 @@ function Resume () {
     return (
         <div className={styles.resumeHighlight}>
             <ResumeBlock name="Communication Professional" color="green" graphic="./assets/comm.png" alt='sattelite in space' description="I have my Master's of Science in Strategic Communication from Brian Lamb School of Communication at Purdue University. Additionally, I have nearly a decade of experience working with a full-service communication department. I have significant experience planning, managing, and delivering successful projects."></ResumeBlock>
-            <ResumeBlock name="Project Management" color="border-color:red" graphic="./assets/pm.png" alt='sattelite in space' description="I have my Project Management Professional (PMP) certification from the PMI. I love managing creative projects and creative people."></ResumeBlock>
+            <ResumeBlock name="Project Management Professional" color="border-color:red" graphic="./assets/pm.png" alt='sattelite in space' description="I have my Project Management Professional (PMP) certification from the PMI. I love managing creative projects and creative people."></ResumeBlock>
             <ResumeBlock name="User Experience Certification" color="yellow" graphic="./assets/ux.png" alt='sattelite in space'description="Not only do I focus on my audience when drafting communication plans, but also when creating solutions. I have a certification for Nielsen Norman Group in UX."></ResumeBlock>
         </div>
     )
