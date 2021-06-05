1b422c9a93f78bda538a53248264b83f7d8487b2
Kenneth Wilson <kenny@meetkenny.com>
2021-06-04 21:57:04 -0400

    optimize images for higher lightspeed score

--- src/templates/category.jsx
+++ src/templates/category.jsx
@@ -9,7 +9,7 @@ const Hero = styled.header`
   background-color: ${(props) => props.theme.colors.primary};
   padding-top: 1rem;
   padding-bottom: 4rem;
-  background-image: url('/bg/space/1.png');
+  background-image: url('/bg/space/1.webp');
   background-attachment: fixed;
   background-position: center;
   background-repeat: no-repeat;
